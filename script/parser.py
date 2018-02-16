c9c81ea85e87018347047ada5d2ea79a7fff9006
lelapenda <leticia.lapenda@oncase.com.br>
2018-02-16 17:35:02 -0300

    Inicio de tratamento para varios sitemaps por pagina (continua)

--- script/parser.py
+++ script/parser.py
@@ -18,12 +18,19 @@ except urllib2.URLError, e:
 
 #Se houver robots, continua:
 robots = response.read()
+robots = robots.replace("Sitemap", "sitemap")
+count_sitemap = robots.count("sitemap")
+
 
 #Verifica se existe Sitemap no Robots.txt
 #ESQUEMA PARA LER 1 SITEMAP, TERIA QUE OTIMIZAR PARA MAIS DE 1
-if 'Sitemap' in robots:
-	sitemap_url = robots.split("Sitemap: ")[1]
+sitemap_urls = []
+
+if 'sitemap' in robots:
+	s = robots.split("sitemap: ")
+	sitemap_urls.append(s[1])
 else:
+	print "Não há Sitemap"
 	exit()
 
 
@@ -31,23 +38,31 @@ else:
 
 #----Sitemap
 
-
-#Visita página do sitemap e recupera links
-try:
-	response = urllib2.urlopen(sitemap_url)
-except urllib2.URLError, e:
-    print 'Não foi possível acessar o \"Sitemap\". Código de erro', e
-    exit()
+all_urls = []
 
 
-sitemap = response.read()
-urls = sitemap.split("<loc>")
+for i in range(0, len(sitemap_urls)):
+	#Visita página do sitemap e recupera links
+	try:
+		response = urllib2.urlopen(sitemap_urls[i])
+	except urllib2.URLError, e:
+	    print 'Não foi possível acessar o \"Sitemap\". Código de erro', e
+	    exit()	
+	
 
-for i in range(1,len(urls)):
-	urls[i] = urls[i].split("</loc>")[0]
+	sitemap = response.read()
+	urls = sitemap.split("<loc>")	
 
-del urls[0] #lista urls contém todos os links do sitemap
+	for i in range(1,len(urls)):
+		urls[i] = urls[i].split("</loc>")[0]	
+
+	del urls[0] #lista urls contém todos os links do sitemap	
+
+
+
+all_urls.append(urls);
 
 #imprimir
-for i in range(0, len(urls)):
-	print urls[i]	
+for i in range(0, len(all_urls)):
+	for j in range (0, len(all_urls[i])):
+		print all_urls[i][j]	

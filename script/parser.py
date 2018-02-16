2cf57adf09cdb11df5dbb1232a70d060b89f6e67
lelapenda <leticia.lapenda@oncase.com.br>
2018-02-16 16:56:22 -0300

    Adição de exceções ao código para caso de o robots.txt ou sitemap não exista. Adição de parâmetro.

--- script/parser.py
+++ script/parser.py
@@ -1,28 +1,53 @@
+#encoding: utf-8
 import urllib2
+import sys
 
-parametro = 'http://www.cartoonnetwork.com.br/robots.txt'
-response = urllib2.urlopen(parametro)
+
+
+#----Robots
+
+
+parametro = sys.argv[1] #exemplo: 'http://www.cartoonnetwork.com.br/robots.txt'
+
+#Verifica se dominio possui Robots.txt
+try:
+	response = urllib2.urlopen(parametro + "/robots.txt")
+except urllib2.URLError, e:
+    print 'Não foi possível acessar o \"Robots.txt\". Código de erro', e
+    exit()
+
+#Se houver robots, continua:
 robots = response.read()
-
 
 #Verifica se existe Sitemap no Robots.txt
 #ESQUEMA PARA LER 1 SITEMAP, TERIA QUE OTIMIZAR PARA MAIS DE 1
 if 'Sitemap' in robots:
 	sitemap_url = robots.split("Sitemap: ")[1]
-	if sitemap_url.startswith("https"):
-		sitemap_url = sitemap_url.split("\r")[0]
-		print sitemap_url
 else:
 	exit()
 
-#Visita pagina do sitemap e recupera links
-response = urllib2.urlopen(sitemap_url)
-sitemap = response.read()
 
+
+
+#----Sitemap
+
+
+#Visita página do sitemap e recupera links
+try:
+	response = urllib2.urlopen(sitemap_url)
+except urllib2.URLError, e:
+    print 'Não foi possível acessar o \"Sitemap\". Código de erro', e
+    exit()
+
+
+sitemap = response.read()
 urls = sitemap.split("<loc>")
 
 for i in range(1,len(urls)):
 	urls[i] = urls[i].split("</loc>")[0]
 
-del urls[0]
-print urls	#lista urls contem todos os links do sitemap
+del urls[0] #lista urls contém todos os links do sitemap
+
+#imprimir
+for i in range(0, len(urls)):
+	print urls[i]	
